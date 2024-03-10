3805415c8980db563412d813cd97130194aca7c9
Prakash Premkumar <prakash.p.2292@gmail.com>
2024-03-10 20:44:59 +0530

    mysql-lib config reading code !

--- go.mod
+++ go.mod
@@ -5,4 +5,6 @@ go 1.18
 require (
 	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/go-sql-driver/mysql v1.8.0 // indirect
+	github.com/pelletier/go-toml/v2 v2.1.1 // indirect
+	github.com/prakash-p-3121/tomllib v0.0.1 // indirect
 )
--- mysql.go
+++ mysql.go
@@ -4,9 +4,29 @@ import (
 	"database/sql"
 	"fmt"
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/prakash-p-3121/tomllib"
+	"io/ioutil"
 )
 
-func CreateDatabaseConnection(cfg MySQLCfg) (*sql.DB, error) {
+func getMySQLCfg(filePath string) (*MySQLCfg, error) {
+	data, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		fmt.Println("Error reading file:", err)
+		return nil, err
+	}
+	var cfg MySQLCfg
+	err = tomllib.Serialize(data, &cfg)
+	if err != nil {
+		return nil, err
+	}
+	return &cfg, nil
+}
+
+func CreateDatabaseConnection(cfgPath string) (*sql.DB, error) {
+	cfg, err := getMySQLCfg(cfgPath)
+	if err != nil {
+		panic(err)
+	}
 	hostPortDatabase := fmt.Sprintf("%s:%d@/%s", cfg.DatabaseName, cfg.Port, cfg.DatabaseName)
 	db, err := sql.Open("mysql", hostPortDatabase)
 	if err != nil {
